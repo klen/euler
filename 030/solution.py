b48fd0bb543b25b784f37f4343ead8b9c2e463fb
Kirill Klenov <horneds@gmail.com>
2014-03-28 01:47:00 +0700

    Solve 29 and 30.

---
+++ 030/solution.py
@@ -0,0 +1,30 @@
+""" Project Euler problem #30. """
+
+
+def problem():
+    """ Solve the problem.
+
+    Surprisingly there are only three numbers that can be written as the sum of
+    fourth powers of their digits:
+
+    1634 = 1**4 + 6**4 + 3**4 + 4**4
+    8208 = 8**4 + 2**4 + 0**4 + 8**4
+    9474 = 9**4 + 4**4 + 7**4 + 4**4
+    As 1 = 1**4 is not a sum it is not included.
+
+    The sum of these numbers is 1634 + 8208 + 9474 = 19316.
+
+    Find the sum of all the numbers that can be written as the sum of fifth
+    powers of their digits.
+
+    Answer: 443839
+
+    """
+    # 9**5 * 6 = 354294 (6 digits last real case)
+    return sum(
+        sum(int(c)**5 for c in str(num)) == num and num or 0
+        for num in range(10, 354294))
+
+
+if __name__ == '__main__':
+    print problem()
